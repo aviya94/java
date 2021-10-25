ea483b3dbb2fca85607948651d0d916001a89217
aviya94 <aviameidan@gmail.com>
2021-10-25 10:00:47 +0300

    fix SortedLinkedList

--- oop/day 08/linked_list/src/DoublyLinkedList/SortedLinkedList.java
+++ oop/day 08/linked_list/src/DoublyLinkedList/SortedLinkedList.java
@@ -6,18 +6,23 @@ import java.util.Iterator;
 public class SortedLinkedList<T> implements Iterable<T> {
     public DoublyLinkedList<T> linkedList;
 
-    public SortedLinkedList()
-    {
-        linkedList=new DoublyLinkedList<T>();
+    public SortedLinkedList() {
+        linkedList = new DoublyLinkedList<T>();
     }
 
     public void add(T obj, Comparator<T> comp) {
 
+        boolean bool = false;
         for (T e : linkedList) {
             if (comp.compare(obj, e) > 0 || comp.compare(obj, e) == 0) {
                 linkedList.addApter(e);
+                bool=true;
             }
 
+        }
+        if(bool==false)
+        {
+            linkedList.addToTail(obj);
         }
     }
 
