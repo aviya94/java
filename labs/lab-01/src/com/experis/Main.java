60f91e036d90ea5c014b3867cf70797b48e82654
aviya94 <aviameidan@gmail.com>
2021-10-27 15:19:03 +0300

    add main class

--- labs/lab-01/src/com/experis/Main.java
+++ labs/lab-01/src/com/experis/Main.java
@@ -3,6 +3,7 @@ package com.experis;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        Menue menue = new Menue();
     }
 }
