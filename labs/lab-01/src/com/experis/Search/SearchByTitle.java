42d9e6e70da45561458b429cf88e1d9a75ecc04c
aviya94 <aviameidan@gmail.com>
2021-10-27 17:41:13 +0300

    fix SearchByTitle class

--- labs/lab-01/src/com/experis/Search/SearchByTitle.java
+++ labs/lab-01/src/com/experis/Search/SearchByTitle.java
@@ -1,109 +1,121 @@
 package com.experis.Search;
 
 import com.experis.LoadDatabase;
-import com.experis.Search.Search;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-
 public class SearchByTitle implements Search {
     private LoadDatabase loadDatabase;
     private ArrayList<String[]> searchResult;
+    private HashMap<String, String[]> resultWithoutReduction;
 
     public SearchByTitle(LoadDatabase loadDatabase) {
         this.loadDatabase = loadDatabase;
     }
 
     public void search(String choice) {
-        initialSearchResult();
-        HashMap<String, String[]> containWordSelected = new HashMap<String, String[]>();
-
+        initialResult();
         String[] choiceWordArr = choice.split(" ");
-
-        addwantedWord(choiceWordArr, containWordSelected);
-        RemoveUnwantedWord(choiceWordArr, containWordSelected);
+        addwantedWord(choiceWordArr);
+        RemoveUnwantedWord(choiceWordArr);
 
     }
 
-    private void initialSearchResult() {
-        searchResult = new ArrayList<String[]>();
-    }
-
-    private void addwantedWord(String[] choiceWordArr, HashMap<String, String[]> containWordSelected) {
-
-        if (CheckOnlyLessWorld(choiceWordArr, containWordSelected) == false) ;
+    private void addwantedWord(String[] choiceWordArr) {
+        if (CheckIfHaveOnlyLessWorld(choiceWordArr) == false) ;
         {
+
             for (String word : choiceWordArr) {
 
-                if (!word.startsWith("-")) {
-
-                    for (Map.Entry book : loadDatabase.getBooksCatalogTitel().entrySet()) {
-                        String key = (String) book.getKey();
-
-                        if (key.contains(word) && containWordSelected.get(key) == null) {
-                            containWordSelected.put((String) book.getKey(), (String[]) book.getValue());
-                            searchResult.add((String[]) book.getValue());
-                        }
-                    }
+                if (!isLess(word)) {
+                    scanCatalogToAddBookToResult(word);
                 }
             }
         }
     }
 
-    private Boolean CheckOnlyLessWorld(String[] choiceWordArr, HashMap<String, String[]> containWordSelected) {
+    private void RemoveUnwantedWord(String[] choiceWordArr) {
+        for (String word : choiceWordArr) {
+            scanCatalogToRemoveFromResult(word);
+        }
+    }
+
+    private void scanCatalogToRemoveFromResult(String word) {
+        for (Map.Entry book : resultWithoutReduction.entrySet()) {
+            String key = (String) book.getKey();
+
+            if (isLess(word)) {
+                String wordWithoutLess = word.substring(1, word.length());
+                removeFromResult(key, wordWithoutLess, book, true);
+
+            } else {
+                removeFromResult(key, word, book, false);
+            }
+
+        }
+    }
+
+    private Boolean CheckIfHaveOnlyLessWorld(String[] choiceWordArr) {
         Boolean onlyLess = true;
+
         for (String word : choiceWordArr) {
             if (!word.startsWith("-")) {
                 onlyLess = false;
             }
         }
+
         if (onlyLess == true) {
-            for (Map.Entry book : loadDatabase.getBooksCatalogTitel().entrySet()) {
-                containWordSelected.put((String) book.getKey(), (String[]) book.getValue());
-                searchResult.add((String[]) book.getValue());
-            }
+            addAllCatalogToResult();
         }
+
         return onlyLess;
     }
 
-    private void RemoveUnwantedWord(String[] choiceWordArr, HashMap<String, String[]> containWordSelected) {
+    private void scanCatalogToAddBookToResult(String word) {
+        for (Map.Entry book : loadDatabase.getBooksCatalogTitel().entrySet()) {
+            String key = (String) book.getKey();
 
-        for (String word : choiceWordArr) {
-
-            if (word.startsWith("-")) {
-
-                for (Map.Entry book : containWordSelected.entrySet()) {
-
-                    String key = (String) book.getKey();
-                    String wordWithoutLess = word.substring(1, word.length());
-
-                    if (key.contains(wordWithoutLess) && searchResult.contains(book.getValue())) {
-                        searchResult.remove((String[]) book.getValue());
-                    }
-                }
-            } else {
-
-                for (Map.Entry book : containWordSelected.entrySet()) {
-                    String key = (String) book.getKey();
-                    if (!key.contains(word) && searchResult.contains(book.getValue())) {
-                        searchResult.remove((String[]) book.getValue());
-                    }
-                }
+            if (key.contains(word) && resultWithoutReduction.get(key) == null) {
+                addToResult(book);
             }
+        }
+    }
 
+    private boolean isLess(String word) {
+        return word.startsWith("-");
+    }
+
+    private void addAllCatalogToResult() {
+        for (Map.Entry book : loadDatabase.getBooksCatalogTitel().entrySet()) {
+            addToResult(book);
+        }
+    }
+
+    private void addToResult(Map.Entry book) {
+        resultWithoutReduction.put((String) book.getKey(), (String[]) book.getValue());
+        searchResult.add((String[]) book.getValue());
+    }
+
+    private void removeFromResult(String key, String word, Map.Entry book, Boolean isContainWord) {
+        if (key.contains(word) == isContainWord && searchResult.contains(book.getValue())) {
+            searchResult.remove((String[]) book.getValue());
         }
     }
 
     public ArrayList<String[]> getResult() {
+
         return searchResult;
     }
 
     public void print() {
+
         if (searchResult.size() == 0) {
             System.out.println("books not found");
+
         } else {
+
             for (String[] line : searchResult) {
                 System.out.print(line[0]);
                 System.out.print("|" + line[1]);
@@ -114,6 +126,11 @@ public class SearchByTitle implements Search {
             }
             System.out.println();
         }
+    }
+
+    private void initialResult() {
+        searchResult = new ArrayList<String[]>();
+        resultWithoutReduction = new HashMap<String, String[]>();
     }
 
 }
