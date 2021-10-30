e4b8c111d71a2f435f17082c41b637297278d9f8
aviya94 <aviameidan@gmail.com>
2021-10-30 17:25:39 +0300

    fix database

--- labs/lab-01/src/com/experis/dataBase/LoadDatabase.java
+++ labs/lab-01/src/com/experis/dataBase/LoadDatabase.java
@@ -1,31 +1,31 @@
-package com.experis;
+package com.experis.dataBase;
+
+import parser.BookParser;
 
 import java.io.*;
 
 public class LoadDatabase {
-    //private FileInputStream fileInputStream;
+
     private BufferedReader bufferedReader;
     private DataBase dataBase;
-    private BookParser parser;
-    private FileInputStream fileInputStream;
 
-    public LoadDatabase(String filePath, BookParser bookPraser,DataBase dataBase)  {
+    public LoadDatabase(String filePath, BookParser bookPraser, DataBase dataBase) {
+
         try {
-        this.dataBase=dataBase;
-        bufferedReader = new BufferedReader(new FileReader(filePath));
-
-
+            this.dataBase = dataBase;
+            bufferedReader = new BufferedReader(new FileReader(filePath));
             addDataToStructuresDataFromFile(bookPraser);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-        parser = bookPraser;
     }
 
     private void loadFile(String filePath) {
 
         try {
             bufferedReader = new BufferedReader(new FileReader(String.valueOf(filePath)));
+
         } catch (FileNotFoundException e) {
             System.out.println("File Not Found");
         }
@@ -44,8 +44,6 @@ public class LoadDatabase {
             String[] filds = bookPraser.parser(line);
             dataBase.addNewBook(filds);
         }
-
-
     }
 
 }
