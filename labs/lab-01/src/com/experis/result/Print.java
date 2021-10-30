f6ea49e8de64b300ae7895dcad7e47bbacdbbf49
aviya94 <aviameidan@gmail.com>
2021-10-30 17:22:58 +0300

    add result class

---
+++ labs/lab-01/src/com/experis/result/Print.java
@@ -0,0 +1,7 @@
+package com.experis.result;
+
+public abstract class Print {
+    public static void print(String toPrint) {
+        System.out.println(toPrint);
+    }
+}
---
+++ labs/lab-01/src/com/experis/result/SearchResult.java
@@ -0,0 +1,48 @@
+package com.experis.result;
+
+import com.experis.dataBase.Book;
+import com.experis.dataBase.DataBase;
+
+import java.util.ArrayList;
+
+public abstract class SearchResult {
+
+    public static String result(Book bookResult, DataBase dataBase) {
+
+        if (bookResult == null) {
+            return null;
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("ISBN: " + bookResult.isbn + "\n");
+        stringBuilder.append("Title: " + dataBase.books.get(bookResult.bookTitle) + "\n");
+        stringBuilder.append("Author: " + dataBase.authors.get(bookResult.bookAuthor) + "\n");
+        stringBuilder.append("Year: " + bookResult.year + "\n");
+        stringBuilder.append("Publisher: " + dataBase.publishers.get(bookResult.publisher));
+        String result = String.valueOf(stringBuilder);
+        return result;
+
+    }
+
+    public static String result(ArrayList<Book> booksResult, DataBase dataBase) {
+
+        if (booksResult.size() == 0) {
+            return null;
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (Book book : booksResult) {
+            stringBuilder.append(book.isbn + "|");
+            stringBuilder.append(dataBase.books.get(book.bookTitle) + "|");
+            stringBuilder.append(dataBase.authors.get(book.bookAuthor) + "|");
+            stringBuilder.append(book.year + "|");
+            stringBuilder.append(dataBase.publishers.get(book.publisher) + "\n");
+        }
+
+        String result = String.valueOf(stringBuilder);
+        return result;
+    }
+
+
+}
