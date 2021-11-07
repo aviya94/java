b8315bfba9424c7653531bb0ea1233627390ee40
Muhammad Zahalqa <m@tryfinally.com>
2021-11-07 09:05:00 +0200

    Code review: reader test

--- labs/messenger/classic/src/com/experis/test/RederTest.java
+++ labs/messenger/classic/src/com/experis/test/RederTest.java
@@ -3,18 +3,28 @@ package com.experis.test;
 import com.experis.sourse.FileReaders;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RederTest {
     FileReaders fileReaders;
 
     @BeforeEach
     void setUp() {
-        fileReaders = new FileReaders("C:\\Users\\user\\course\\java\\labs\\messenger\\classic\\src\\com\\experis\\resources\\Read.txt");
+        final var path = "./src/com/experis/resources/Read.txt";
+        fileReaders = new FileReaders(path);
     }
 
     @Test
     void writing_to_file() {
-        String result =fileReaders.read();
-assertEquals("hellow word!\nhellow word!",result);
+        final var expected = """
+                Hello World!
+                Love Java?
+                """;
+
+        String result = fileReaders.read();
+        assertEquals(expected, result);
     }
+
+
 }
